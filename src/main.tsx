44616d43bd29220d386ae27c3456e795493ca561
Павел <pavel@qq-2.local>
2025-02-20 16:26:46 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'
+import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
+const manifestUrl = 'https://raw.githubusercontent.com/markokhman/func-course-chapter-5-code/master/public/manifest.json'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>,
+)
